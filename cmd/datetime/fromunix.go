a7755609bc9fe275c96b484b931ba24377f43f81
Renato Torres <renato.torres@outsystems.com>
2021-12-03 16:06:07 +0000

    feat: add datetime/fromunix

--- cmd/datetime/fromunix.go
+++ cmd/datetime/fromunix.go
@@ -14,31 +14,43 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
-package programming
+package datetime
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/MakeNowJust/heredoc"
-	"github.com/google/uuid"
 	"github.com/renato0307/canivete/pkg/iostreams"
 	"github.com/spf13/cobra"
 )
 
-func NewUuidCmd(iostreams iostreams.IOStreams) *cobra.Command {
-	var uuidCmd = &cobra.Command{
-		Use:   "uuid",
-		Short: "Generates UUIDs (or GUIDs)",
+func NewFromUnixCmd(iostreams iostreams.IOStreams) *cobra.Command {
+	var fromUnixCmd = &cobra.Command{
+		Use:   "fromunix",
+		Short: "Converts a Unix timestamp to human friendly format",
 		Long: heredoc.Doc(`
-			UUID also known as GUID is a 16 byte or 128-bit number.
-			It is meant to uniquely identify something.
+			Converts a Unix timestamp to human friendly format.
+
+			The Unix timestamp is a way to track time as a running total of seconds.
+			This count starts at the Unix Epoch on January 1st, 1970 at UTC.
 		`),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			_, err := fmt.Fprintln(iostreams.Out, uuid.New())
+
+			unixTime, _ := cmd.Flags().GetInt64("value")
+			t := time.Unix(unixTime, 0)
+			strDate := t.Format(time.UnixDate)
+			_, err := fmt.Fprintln(iostreams.Out, strDate)
 
 			return err
 		},
+		Example: heredoc.Doc(`
+			canivete datetime fromunix --value 1638964800
+			canivete datetime fromunix -v 1638964800`),
 	}
 
-	return uuidCmd
+	fromUnixCmd.Flags().Int64P("value", "v", 0, "the unix timestamp")
+	fromUnixCmd.MarkFlagRequired("value")
+
+	return fromUnixCmd
 }
