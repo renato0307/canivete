44ddb460df9e4fbc45ae0a009891ef10ed9ef020
Renato Torres <renato.torres@outsystems.com>
2021-12-03 15:12:16 +0000

    test: add finance related tests

---
+++ cmd/finance/compoundInterests_test.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2021 Renato Torres <renato.torres@pm.me>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package finance
+
+import (
+	"testing"
+
+	"github.com/renato0307/canivete/pkg/iostreams"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewCompoundInterestsCmd(t *testing.T) {
+	// arrange
+	iostreams, _, out, _ := iostreams.Test()
+	cmd := NewCompoundInterestsCmd(*iostreams)
+
+	// act
+	cmd.SetArgs([]string{
+		"--time=10",
+		"--invest-amount=1000",
+		"--annual-interest-rate=5",
+		"--compound-periods=12",
+	})
+	_, err := cmd.ExecuteC()
+
+	// assert
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Contains(t, out.String(), "1647.01")
+}
---
+++ cmd/finance/finance_test.go
@@ -0,0 +1,38 @@
+/*
+Copyright © 2021 Renato Torres <renato.torres@pm.me>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package finance
+
+import (
+	"testing"
+
+	"github.com/renato0307/canivete/pkg/iostreams"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewFinanceCmd(t *testing.T) {
+	// arrange
+	iostreams, _, _, _ := iostreams.Test()
+	cmd := NewFinanceCmd(*iostreams)
+
+	// act
+	_, err := cmd.ExecuteC()
+
+	// assert
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "must specify a subcommand")
+	assert.Len(t, cmd.Commands(), 3)
+}
